58347d31386d9ea1b68fce45295a54eb7bc98617
csy0427 <tuchsoo2@naver.com>
2017-08-11 17:41:27 +0900

    final

--- src/AddComment.java
+++ src/AddComment.java
@@ -5,6 +5,11 @@ public class AddComment implements BoardFunction {
     IOManager manager=new IOManager();
     @Override
     public void sol(Map<String,Map<String,String>> list, List<String> boardnumber){
-        manager.addComment(list,boardnumber);
+        ///manager.addComment(list,boardnumber);
+        manager.PrintList(list, boardnumber);
+        String id=manager.inputBoardNumber();
+        System.out.println("Enter the comment");
+        String tmp_comment=input.nextLine();
+        bm.addComment(id,tmp_comment);
     }
 }
