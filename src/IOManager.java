e3527337be058d5a0cd021275c0e2d8ac15f8f3e
csy0427 <tuchsoo2@naver.com>
2017-08-16 12:51:34 +0900

    iomanager

--- src/IOManager.java
+++ src/IOManager.java
@@ -9,11 +9,6 @@ public class IOManager {
     Map<String,Map<String,String>> list;
     List<String> boardnumber;
 
-    public IOManager(Map<String,Map<String,String>> list, List<String> boardnumber){
-        this.list=list;
-        this.boardnumber=boardnumber;
-    }
-
     public  void PrintFirst(){
         System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
         System.out.println("1. 게시판 리스트");
@@ -29,78 +24,4 @@ public class IOManager {
 
     }
 
-    /*public void PrintList(){
-        updateBoard();
-        for(int i=0;i<boardnumber.size();i++){
-            String key=boardnumber.get(i); //key 획득
-            System.out.println("     게시글 번호       |          제목          ");
-            System.out.printf("     %s                   %s\n",key, list.get(key).get("title"));
-        }
-    }
-
-    public String inputBoardNumber(){
-        System.out.println("Enter the board number");
-        String id=input.nextLine();
-        return id;
-    }
-
-    public void getPost(){
-        updateBoard();
-        PrintList();
-        String id=inputBoardNumber();
-        Map<String,String> tmp=bm.getBoardPost(id);
-        comments=bm.getComment(id);
-        System.out.println("   게시글 번호   |          제목           ");
-        System.out.printf("    %s              %s\n", id, tmp.get("title"));
-        System.out.println("   내용");
-        System.out.println(tmp.get("content"));
-        System.out.printf("조회수: %s\n",tmp.get("views"));
-        System.out.println("댓글 : ");
-        if(comments.size()==0) return;
-        for(int i=0;i<comments.size();i++){
-            System.out.println(i+1 +" 번째 댓글 :  " +comments.get(i));
-        }
-    }
-
-    public void addPost(){
-        System.out.print("제목 :  ");
-        String tmp_title= input.nextLine();
-        System.out.print("내용 :  ");
-        String tmp_content= input.nextLine();
-        bm.addPost(tmp_title,tmp_content);
-        updateBoard();
-    }
-
-    public void modifyPost(){
-        PrintList();
-        String id=inputBoardNumber();
-        System.out.print("수정 제목 :  ");
-        String tmp_title= input.nextLine();
-        System.out.print("수정 내용 :  ");
-        String tmp_content= input.nextLine();
-        bm.modifyPost(id,tmp_title,tmp_content);
-        updateBoard();
-    }
-
-    public void deletePost(){
-        PrintList();
-        String id=inputBoardNumber();
-        bm.deletePost(id);
-        updateBoard();
-    }
-
-    public void addComment(){
-        PrintList();
-        String id=inputBoardNumber();
-        System.out.println("Enter the comment");
-        String tmp_comment=input.nextLine();
-        bm.addComment(id,tmp_comment);
-        updateBoard();
-    }
-
-    public void updateBoard(){
-        list= bm.getBoardList();
-        boardnumber=bm.getBoardNumber();
-    }
-    */
 }
