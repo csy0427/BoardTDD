84b2c797bad9d0780db429c5408351ffa7a2e7c8
csy0427 <tuchsoo2@naver.com>
2017-08-17 13:53:41 +0900

    test refactoring2

--- test/BoardTest.java
+++ test/BoardTest.java
@@ -33,61 +33,43 @@ public class BoardTest {
         totalSize++;
     }
 
-    @Test(expected = NullPointerException.class)
+    @Test
     public void 게시글읽기() throws Exception {
         Map<String, String> tmp = bm.getBoardPost(key[0]);
         assertEquals(title[0], tmp.get("title"));
         assertEquals(content[0], tmp.get("content"));
-
-        //잘못된 게시판 번호 입력
-        fault();
     }
 
-    @Test(expected = NullPointerException.class)
+    @Test
     public void 게시글수정() {
         bm.modifyPost(key[0], "567", "def");
         assertEquals("567", bm.getBoardPost(key[0]).get("title"));
         assertEquals("def", bm.getBoardPost(key[0]).get("content"));
-
-        //잘못된 게시판 번호 입력
-        fault();
     }
 
-    @Test(expected = NullPointerException.class)
+    @Test
     public void 게시글삭제() {
         assertEquals(1, bm.getPostNumber());
         bm.deletePost(key[0]);
         assertEquals(0, bm.getPostNumber());
         totalSize--;
-
-        //잘못된 게시판 번호 입력
-        fault();
     }
 
-    @Test(expected = NullPointerException.class)
+    @Test
     public void 조회수증가() {
         assertEquals("1", tmp_map.get("views"));
         tmp_map = bm.getBoardPost(key[0]);
         assertEquals("2", tmp_map.get("views"));
-
-        //잘못된 게시판 번호 입력
-        fault();
     }
 
-    @Test(expected = NullPointerException.class)
+    @Test
     public void 게시글리스트() {
         List<String> boardNumber = bm.getBoardNumber();
         for (int i = 0; i < boardNumber.size(); i++) {
             assertEquals(title[i], bm.getBoardPost(key[i]).get("title"));
             assertEquals(content[i], bm.getBoardPost(key[i]).get("content"));
         }
-        //만약 아무글도 없다면
-        게시글삭제();
-        for (int i = 0; i < boardNumber.size(); i++) {
-            System.out.println(title[i]);
-            assertEquals(title[i], bm.getBoardPost(key[i]).get("title"));
-            assertEquals(content[i], bm.getBoardPost(key[i]).get("content"));
-        }
+
     }
 
     /*
@@ -95,17 +77,15 @@ public class BoardTest {
     *
     * */
 
-    @Test(expected = NullPointerException.class)
+    @Test
     public void 댓글달기() {
         bm.addComment(key[0], "이 글 좋네요!");
         List<String> tmp_comment = bm.getComment(key[0]);
         assertEquals("이 글 좋네요!", tmp_comment.get(0));
-
-        //예외 발생 예상 구간
-        fault();
     }
 
-    public void fault(){
+    @Test(expected = NullPointerException.class)
+    public void 잘못된게시글번호입력(){
         bm.getBoardPost("123");
     }
 
